789776b1ddd3e46957cefeb39b66c32719b56e1d
Den-od <denis.kopylov.gm@gmail.com>
2022-06-03 23:43:20 +0300

    №4 commit подправил вывод текста

--- src/Loopy.java
+++ src/Loopy.java
@@ -1,13 +1,13 @@
 public class Loopy {
     public static void main (String[] args) {
         int x = 1;
-        System.out.println ("Перед началом цикла");
+        System.out.println ("Цикл начался");
         while (x < 4){
-            System.out.println ("Внутри цикла");
+            System.out.println ("Цикл выполняется пока Х меньше четырёх. Х увеличивается на единицу");
             System.out.println ("Значение x равно" + x);
             x = x + 1;
         }
-        System.out.println ("После окончания цикла") ;
+        System.out.println ("Цикл прекратился, т.к. условие x < 4 не выполняется") ;
     }
 
 }
